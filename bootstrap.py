352b48797a3549ed29475cf2a5144b7057899fe8
Raghav Garimella <105839193+Rumbl3S@users.noreply.github.com>
2025-09-18 18:14:37 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,24 +1,41 @@
-import os
-
-from app import app, db, DB_FILE
-
-from models import *
+import json
+from app import app
+from models import db, Club, Tag, User
 
 def create_user():
-    print("TODO: Create a user called josh")
+    # create Josh
+    josh = User(username="josh", email="josh@example.com")
+    db.session.add(josh)
+    db.session.commit()
 
-def load_data():
-    print("TODO: Load in clubs.json to the database.")
+def load_json():
+    with open("clubs.json") as f:
+        clubs_data = json.load(f)
 
+    for club_json in clubs_data:
+        club = Club(
+            name=club_json.get("name"),
+            description=club_json.get("description"),
+            website=club_json.get("website"),
+            email=club_json.get("email"),
+        )
 
-# No need to modify the below code.
+        # handle tags
+        for tag_name in club_json.get("tags", []):
+            tag = Tag.query.filter_by(name=tag_name).first()
+            if not tag:
+                tag = Tag(name=tag_name)
+                db.session.add(tag)
+            club.tags.append(tag)
+
+        db.session.add(club)
+
+    db.session.commit()
+
 if __name__ == "__main__":
-    # Delete any existing database before bootstrapping a new one.
-    LOCAL_DB_FILE = "instance/" + DB_FILE
-    if os.path.exists(LOCAL_DB_FILE):
-        os.remove(LOCAL_DB_FILE)
-
     with app.app_context():
+        db.drop_all()
         db.create_all()
         create_user()
-        load_data()
+        load_json()
+        print("Database bootstrapped with user + clubs")
